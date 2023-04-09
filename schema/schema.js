c3a75c61abc9723bf7c58876d62d4a3c4e266d7f
blac <badejo.semilore.7@gmail.com>
2023-04-09 11:17:37 +0100

    implemented titlecase on the names

--- schema/schema.js
+++ schema/schema.js
@@ -3,6 +3,7 @@ const axios = require("axios");
 const levenshtein = require("js-levenshtein");
 
 const User = require("../models/userModel");
+const toTitleCase = require("../utils/titleCase");
 
 const {
   GraphQLObjectType,
@@ -72,11 +73,53 @@ const RootQuery = new GraphQLObjectType({
       },
       resolve(parent, args) {
         const { user_account_number, user_bank_code } = args;
+
         return User.findOne({
           where: {
             user_account_number,
             user_bank_code,
           },
+        }).then((user) => {
+          if (!user) {
+            return null;
+          }
+
+          if (user.user_account_name) {
+            // User has provided their own account name
+            return {
+              ...user.dataValues,
+              user_account_name: toTitleCase(user.user_account_name),
+            };
+          } else {
+            // Call Paystack API to resolve account name
+            const options = {
+              method: "GET",
+              url: "https://api.paystack.co/bank/resolve",
+              params: {
+                account_number: user.user_account_number,
+                bank_code: user.user_bank_code,
+              },
+              headers: {
+                Authorization:
+                  "Bearer sk_test_b7d166703d2c8883b42f764b528ce128a114743f",
+              },
+            };
+
+            return axios(options)
+              .then((response) => {
+                const resolvedAccountName = toTitleCase(
+                  response.data.data.account_name
+                );
+                return {
+                  ...user.dataValues,
+                  user_account_name: resolvedAccountName,
+                };
+              })
+              .catch((error) => {
+                console.error(error);
+                return null;
+              });
+          }
         });
       },
     },
