fee7a702f8db7b5b3d192468e33b5c12ff8e50ba
blac <badejo.semilore.7@gmail.com>
2023-04-12 21:25:49 +0100

    updated test suites

--- app.test.js
+++ app.test.js
@@ -5,7 +5,7 @@ describe("GraphQL API", () => {
   // test("Levenshtein distance is less than or equals 2, should create user account successfully", async () => {
   //   const query = `
   //   mutation {
-  //     addUser(user_account_number: 3122793265, user_bank_code: "011", user_account_name: "ABUBAKRI OLAYIDE ABDUL-LATEEF") {
+  //     addUser(user_account_number: "3122793265", user_bank_code: "011", user_account_name: "ABUBAKRI OLAYIDE ABDUL-LATEEF") {
   //      user_account_number
   //      user_bank_code
   //      user_account_name
@@ -23,7 +23,7 @@ describe("GraphQL API", () => {
   //   expect(response.body).toEqual({
   //     data: {
   //       addUser: {
-  //         user_account_number: 3122793265,
+  //         user_account_number: "3122793265",
   //         user_bank_code: "011",
   //         user_account_name: "ABUBAKRI OLAYIDE ABDUL-LATEEF",
   //         is_verified: true,
@@ -36,7 +36,7 @@ describe("GraphQL API", () => {
   test("Levenshtein distance is greater than 2, should not create user account and return error", async () => {
     const query = `
     mutation {
-      addUser(user_account_number: 3122793265, user_bank_code: "011", user_account_name: "ABUBAKRI ABDUL-LATEEF OLAYIDE") {
+      addUser(user_account_number: "3122793265", user_bank_code: "011", user_account_name: "ABUBAKRI ABDUL-LATEEF OLAYIDE") {
        user_account_number
        user_bank_code
        user_account_name
@@ -54,7 +54,7 @@ describe("GraphQL API", () => {
     expect(response.body).toEqual({
       data: {
         addUser: {
-          user_account_number: 3122793265,
+          user_account_number: "3122793265",
           user_bank_code: "011",
           user_account_name: "ABUBAKRI ABDUL-LATEEF OLAYIDE",
           is_verified: false,
@@ -67,7 +67,7 @@ describe("GraphQL API", () => {
   test("should return user account details when queried with valid account number and bank code", async () => {
     const query = `
       query {
-        user(user_account_number:3122793265, user_bank_code: "011") {
+        user(user_account_number:"3122793265", user_bank_code: "011") {
           user_account_name
           user_account_number
           id
@@ -83,9 +83,9 @@ describe("GraphQL API", () => {
       data: {
         user: {
           user_account_name: "Abubakri Olayide Abdul-lateef",
-          user_account_number: 3122793265,
+          user_account_number: "3122793265",
           id: expect.any(String),
-          is_verified: expect.any(String),
+          is_verified: expect.any(Boolean),
         },
       },
     });
@@ -94,7 +94,7 @@ describe("GraphQL API", () => {
   test("should return an error when queried with an incomplete account number", async () => {
     const query = `
       query {
-        user(user_account_number: 1234, user_bank_code: "058") {
+        user(user_account_number: "1234", user_bank_code: "058") {
           user_account_name
           user_account_number
           id
@@ -123,7 +123,7 @@ describe("GraphQL API", () => {
   test("should return an error when queried with an account number that doesnt exist", async () => {
     const query = `
       query {
-        user(user_account_number: 1234567890, user_bank_code: "058") {
+        user(user_account_number: "1234567890", user_bank_code: "058") {
           user_account_name
           user_account_number
           id
